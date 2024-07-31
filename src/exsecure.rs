cacf8f353071b15a3a44275996aef086c4b228f9
Tran Ngoc Hung <tran.ngoc.hung@volvocars.com>
2024-07-31 12:21:40 +0700

    Add random padding to AES128-CTR to make more secure

--- src/exsecure.rs
+++ src/exsecure.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use ctr::cipher::{KeyIvInit, StreamCipher};
 use aes::cipher::generic_array::GenericArray;
 use sha2::{Sha256, Digest};
+use rand::Rng;
 
 type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
 
@@ -20,26 +21,51 @@ fn hash_aes_key(password: &str) -> [u8; 16] {
     key
 }
 
+// Function to encrypt data with AES-128 CTR mode, adding 4 random bytes at the end
 fn encrypt_aes128_ctr(data_to_encrypt: &[u8], iv_bytes: &[u8], key: &[u8]) -> Result<Vec<u8>, io::Error> {
     if iv_bytes.len() != 16 {
+        return Err(Error::new(ErrorKind::InvalidInput, "wrong IV length"));
+    }
+    if key.len() != 16 {
         return Err(Error::new(ErrorKind::InvalidInput, "wrong key length"));
     }
 
-    let mut encrypted_data = data_to_encrypt.to_vec();
-    let mut cipher = Aes128Ctr64LE::new(GenericArray::from_slice(&key), GenericArray::from_slice(&iv_bytes));
+    // Generate 4 random bytes
+    let mut rng = rand::thread_rng();
+    let mut random_bytes = [0u8; 4];
+    rng.fill(&mut random_bytes);
+
+    // Append the random bytes to the end of the data
+    let mut data_with_random = Vec::from(data_to_encrypt);
+    data_with_random.extend_from_slice(&random_bytes);
+
+    // Perform AES-128 CTR encryption
+    let mut encrypted_data = data_with_random.clone();
+    let mut cipher = Aes128Ctr64LE::new(GenericArray::from_slice(key), GenericArray::from_slice(iv_bytes));
     cipher.apply_keystream(&mut encrypted_data);
 
     Ok(encrypted_data)
 }
 
+// Function to decrypt data with AES-128 CTR mode, removing the last 4 bytes
 fn decrypt_aes128_ctr(encrypted_data: &[u8], iv_bytes: &[u8], key: &[u8]) -> Result<Vec<u8>, io::Error> {
     if iv_bytes.len() != 16 {
+        return Err(Error::new(ErrorKind::InvalidInput, "wrong IV length"));
+    }
+    if key.len() != 16 {
         return Err(Error::new(ErrorKind::InvalidInput, "wrong key length"));
     }
 
+    // Perform AES-128 CTR decryption
     let mut decrypted_data = encrypted_data.to_vec();
-    let mut cipher = Aes128Ctr64LE::new(GenericArray::from_slice(&key), GenericArray::from_slice(&iv_bytes));
+    let mut cipher = Aes128Ctr64LE::new(GenericArray::from_slice(key), GenericArray::from_slice(iv_bytes));
     cipher.apply_keystream(&mut decrypted_data);
+
+    // Remove the last 4 bytes (which are the random bytes)
+    if decrypted_data.len() < 4 {
+        return Err(Error::new(ErrorKind::InvalidInput, "Data too short"));
+    }
+    decrypted_data.truncate(decrypted_data.len() - 4);
 
     Ok(decrypted_data)
 }
